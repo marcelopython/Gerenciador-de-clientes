c619692febaa70a1415534e589b9f5d6028ce151
Marcelo Silva <msr88957@gmail.com>
2021-04-02 12:45:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
 $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
-//var_dump('Location: '.$protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'public');
-//echo '<pre>';
-//var_dump($_SERVER);
 header('Location: '.$protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'public');
